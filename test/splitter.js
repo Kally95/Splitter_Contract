d7743b83c1e8f6297d7da72b11f5b5e5a92c4f54
Ali Kalkanel <Kally-95@hotmail.com>
2020-02-10 18:41:04 +0000

    Added web3.utils.toBN

--- test/splitter.js
+++ test/splitter.js
@@ -5,7 +5,6 @@ contract("Splitter", (accounts) => {
   
   const { BN } = web3.utils;
   const amount = new BN(1000);
-  console.log(amount.toString(10))
   const [owner, recipient1, recipient2] = accounts;
   const recipient1Duplicate = recipient1; 
   const zeroAdd = 0x0000000000000000000000000000000000000000;
@@ -18,7 +17,6 @@ contract("Splitter", (accounts) => {
   describe("Testing contract owner", function() {
     it("The deployer should be the Owwer", async () => {
       const contractOwner = await contractInstance.getOwner({from: owner});
-      console.log(owner)
       assert.strictEqual(contractOwner, owner, "Sender is not Owner");
     });
   });
@@ -27,10 +25,10 @@ contract("Splitter", (accounts) => {
 
     it("Should check balance after Split", async () => {
       await contractInstance.split(recipient1, recipient2, {from: owner, value: amount})
-      const bobBalance = await contractInstance.balances(recipient1, {from: owner});
-      const carolBalance = await contractInstance.balances(recipient2, {from: owner});
-      assert.strictEqual(bobBalance.toString(10), "500", "Bob did not recieve the correct amount")
-      assert.strictEqual(carolBalance.toString(10), "500", "Carol did not recieve the correct amount")
+      const recipient1Balance = await contractInstance.balances(recipient1, {from: owner});
+      const recipient2Balance = await contractInstance.balances(recipient2, {from: owner});
+      assert.strictEqual(recipient1Balance.toString(10), "500", "recipient1 did not recieve the correct amount")
+      assert.strictEqual(recipient2Balance.toString(10), "500", "recipient2 did not recieve the correct amount")
     });
 
     it("Split should fire an event when executed", async () => {
@@ -68,6 +66,8 @@ contract("Splitter", (accounts) => {
     })
 
     it("Should not allow me to use split when kill() is called", async () => {
+      const result = await contractInstance.split(recipient1, recipient2, {from: owner, value: amount});
+      assert.strictEqual(result.logs[0].event, 'LogSplit', "Event 'Split' did't fire");
       await contractInstance.stop({from: owner});
       await contractInstance.kill({from: owner});
       await utils.shouldThrow(contractInstance.split(recipient1, recipient2, {from: owner, value: amount}));
@@ -84,13 +84,13 @@ contract("Splitter", (accounts) => {
 
   describe("Testing withdraw function", function() {
 
-    it("Should allow Bob and Carol to withdraw their split amount", async () => {
+    it("Should allow recipient1 and recipient2 to withdraw their split amount", async () => {
       await contractInstance.split(recipient1, recipient2, {from: owner, value: amount});
-      const bobWithdraw = await contractInstance.withdraw("500", {from: recipient1});
-      const carolWithdraw = await contractInstance.withdraw("500", {from: recipient2});
+      const recipient1Withdraw = await contractInstance.withdraw("500", {from: recipient1});
+      const recipient2Withdraw = await contractInstance.withdraw("500", {from: recipient2});
 
-      assert.isTrue(bobWithdraw.receipt.status, true, "Status did not return true" );
-      assert.isTrue(carolWithdraw.receipt.status, true, "Status did not return true");
+      assert.isTrue(recipient1Withdraw.receipt.status, true, "Status did not return true" );
+      assert.isTrue(recipient2Withdraw.receipt.status, true, "Status did not return true");
     });
 
     it("Should not allow 0 value withdrawals", async () => {
@@ -103,7 +103,7 @@ contract("Splitter", (accounts) => {
     });
 
     it("Should not allow withdrawals greater than callers balance", async () => {
-      const result = await contractInstance.split(recipient1, recipient2, {from: owner, value: amount});
+      await contractInstance.split(recipient1, recipient2, {from: owner, value: amount});
 
       await contractInstance.balances(recipient1, {from: owner});
 
@@ -116,7 +116,7 @@ contract("Splitter", (accounts) => {
       const result = await contractInstance.withdraw("500", {from: recipient1});
       assert.strictEqual(result.receipt.logs[0].event, "LogWithdrawCalled", "Withdraw event did not fire");
       assert.strictEqual(result.receipt.logs[0].args.__length__, 2, "Withdraw should have emitted one event");
-      assert.strictEqual(result.receipt.logs[0].args._withdrawer, recipient1, "Bob was not the withdrawer");
+      assert.strictEqual(result.receipt.logs[0].args._withdrawer, recipient1, "recipient1 was not the withdrawer");
       assert.strictEqual(result.receipt.logs[0].args._withdrawAmount.toString(10), "500", "Did not withdraw the correct amount");
     });
 
@@ -141,15 +141,15 @@ contract("Splitter", (accounts) => {
 
       assert.strictEqual(txObj.receipt.status, true, "withdraw event was not true");
 
-      let hash = txObj.receipt.transactionHash; 
-      let tx = await web3.eth.getTransaction(hash); 
-      let gasUsed = txObj.receipt.gasUsed;
-      let gasPrice = tx.gasPrice;
-      let txFee = new BN(gasUsed * gasPrice);
-      let balanceNow = new BN(await web3.eth.getBalance(recipient1));        
-      let receiveAmount = sendAmount / 2;
+      const hash = txObj.receipt.transactionHash; 
+      const tx = await web3.eth.getTransaction(hash); 
+      const gasUsed = new BN(txObj.receipt.gasUsed);
+      const gasPrice = new BN(tx.gasPrice);
+      const txFee = new BN(gasUsed * gasPrice);
+      const balanceNow = new BN(await web3.eth.getBalance(recipient1));        
+      const receiveAmount = new BN(sendAmount / 2 );
 
-      assert.strictEqual(balanceNow.toString(10), startBalance.add(receiveAmount).sub(txFee).toString(10), "Bob's balance did not return as intended");
+      assert.strictEqual(balanceNow.toString(10), startBalance.add(receiveAmount).sub(txFee).toString(10), "recipient1's balance did not return as intended");
 
     });
 
